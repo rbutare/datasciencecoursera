463590f9c5d0cc1eba0e7f5d4615a0422c507f80
rbutare <remyndayizeye@gmail.com>
2020-07-30 12:26:15 -0400

    Programming assignment 2 answer

---
+++ ProgrammingAssignment2-master/cachematrix.R
@@ -0,0 +1,39 @@
+## The general process is always the same: 
+##run the chunk of code once, 
+##store the output to disk, 
+##and load it up the next time the same chunk is run
+
+
+
+makeCacheMatrix <- function(x = matrix()) {
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) i <<- inverse
+  getinverse <- function() i
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+}
+
+
+
+## This function computes the inverse of the special "matrix" returned 
+## by makeCacheMatrix above. If the inverse has already been calculated 
+## (and the matrix has not changed), then cacheSolve should retrieve 
+## the inverse from the cache.
+
+cacheSolve <- function(x, ...) {
+  i <- x$getinverse()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(i)
+  i
+}
